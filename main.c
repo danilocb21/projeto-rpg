84688db4cbd7a918e4a296c903997eac640bc17b
Soulliam <williamdants62@gmail.com>
2025-09-04 12:59:51 -0300

    Justified all assets resolution for easier hitbox configuration.

--- main.c
+++ main.c
@@ -7,7 +7,30 @@
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
 
-int main(int argc, char* argv[]) {
+SDL_Texture* criarTextura(SDL_Renderer* render, const char* dir)
+{
+    SDL_Surface* surface = IMG_Load(dir);
+    if(!surface) {
+        SDL_Log("Erro ao carregar imagem '%s': '%s'", dir, IMG_GetError());
+        return NULL;
+    }
+
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(render, surface);
+    if(!texture) {
+        SDL_Log("Erro ao criar textura: '%s'", SDL_GetError());
+        return NULL;
+    }
+    SDL_FreeSurface(surface);
+    return texture;
+}
+
+typedef struct {
+    SDL_Texture* texture;
+    SDL_Rect colision;
+} Character;
+
+int main(int argc, char* argv[])
+{
     (void)argc; (void)argv;
 
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
@@ -19,7 +42,7 @@ int main(int argc, char* argv[]) {
         "Tela Preta",
         SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
         SCREEN_WIDTH, SCREEN_HEIGHT,
-        SDL_WINDOW_SHOWN | SDL_WINDOW_MAXIMIZED |SDL_WINDOW_RESIZABLE
+        SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE
     );
     if (!window) {
         fprintf(stderr, "SDL_CreateWindow failed: %s\n", SDL_GetError());
@@ -39,6 +62,11 @@ int main(int argc, char* argv[]) {
     SDL_bool running = SDL_TRUE;
     SDL_Event event;
 
+    // OBJETOS:
+    SDL_Texture* meneghetti_texture = criarTextura(renderer, "assets/sprites/characters/sprite-meneghetti-stopped.png");
+    SDL_Rect meneghetti_hitbox = {(SCREEN_WIDTH / 2) - 16, (SCREEN_HEIGHT / 2) - 16, 32, 32};
+    Character meneghetti = {meneghetti_texture, meneghetti_hitbox};
+
     while (running) {
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT) running = SDL_FALSE;
@@ -47,6 +75,9 @@ int main(int argc, char* argv[]) {
 
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
+
+        SDL_RenderCopy(renderer, meneghetti.texture, NULL, &meneghetti.colision);
+
         SDL_RenderPresent(renderer);
 
         SDL_Delay(16);
