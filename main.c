aa1d2b7255f669b11e665a7813a936195c9c9aff
Soulliam <williamdants62@gmail.com>
2025-09-05 00:02:22 -0300

    Added SDL2 TTF and MIXER libs, also a basic - but functional - walking in-game system.

--- main.c
+++ main.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <math.h>
+// SDL2:
 #include "SDL2/SDL.h"
 #include "SDL2/SDL_image.h"
+#include "SDL2/SDL_ttf.h"
+#include "SDL2/SDL_mixer.h"
 
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
+
 
 SDL_Texture* criarTextura(SDL_Renderer* render, const char* dir)
 {
@@ -39,7 +43,7 @@ int main(int argc, char* argv[])
     }
 
     SDL_Window *window = SDL_CreateWindow(
-        "Tela Preta",
+        "C-Tale: Meneghetti VS Python",
         SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
         SCREEN_WIDTH, SCREEN_HEIGHT,
         SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE
@@ -59,24 +63,98 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-    SDL_bool running = SDL_TRUE;
+    bool running = SDL_TRUE;
+    bool w_pressed = false, s_pressed = false, a_pressed = false, d_pressed = false, spc_pressed = false;
     SDL_Event event;
 
     // OBJETOS:
     SDL_Texture* meneghetti_texture = criarTextura(renderer, "assets/sprites/characters/sprite-meneghetti-stopped.png");
-    SDL_Rect meneghetti_hitbox = {(SCREEN_WIDTH / 2) - 16, (SCREEN_HEIGHT / 2) - 16, 32, 32};
+    SDL_Rect meneghetti_hitbox = {(SCREEN_WIDTH / 2) - 10, (SCREEN_HEIGHT / 2) - 16, 19, 32};
     Character meneghetti = {meneghetti_texture, meneghetti_hitbox};
 
+    SDL_Texture* scenario_texture = criarTextura(renderer, "assets/sprites/scenario/scenario.png");
+    SDL_Rect scenario_hitbox = {0, -SCREEN_HEIGHT, SCREEN_WIDTH * 2, SCREEN_HEIGHT * 2};
+    Character scenario = {scenario_texture, scenario_hitbox};
+
+    int speed = 2;
     while (running) {
         while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) running = SDL_FALSE;
-            else if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE) running = SDL_FALSE;
+            switch(event.type) {
+                case(SDL_QUIT):
+                    running = false;
+                    break;
+                case(SDL_KEYDOWN):
+                    switch(event.key.keysym.sym) {
+                        case(SDLK_w):
+                            w_pressed = true;
+                            break;
+                        case(SDLK_s):
+                            s_pressed = true;
+                            break;
+                        case(SDLK_a):
+                            a_pressed = true;
+                            break;
+                        case(SDLK_d):
+                            d_pressed = true;
+                            break;
+                        case(SDLK_SPACE):
+                            spc_pressed = true;
+                            break;
+                        case(SDLK_ESCAPE):
+                            running = false;
+                    }
+                    break;
+                case(SDL_KEYUP):
+                    switch(event.key.keysym.sym) {
+                        case(SDLK_w):
+                            w_pressed = false;
+                            break;
+                        case(SDLK_s):
+                            s_pressed = false;
+                            break;
+                        case(SDLK_a):
+                            a_pressed = false;
+                            break;
+                        case(SDLK_d):
+                            d_pressed = false;
+                            break;
+                        case(SDLK_SPACE):
+                            spc_pressed = false;
+                    }
+                    break;
+            }
         }
 
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
 
+        SDL_RenderCopy(renderer, scenario.texture, NULL, &scenario.colision);
         SDL_RenderCopy(renderer, meneghetti.texture, NULL, &meneghetti.colision);
+
+        if(w_pressed && scenario.colision.y < 0 && meneghetti.colision.y < (SCREEN_HEIGHT / 2) - 16) {
+            scenario.colision.y += speed;            
+        }
+        else if(w_pressed && meneghetti.colision.y > 0) {
+            meneghetti.colision.y -= speed;
+        }
+        if(s_pressed && scenario.colision.y > SCREEN_HEIGHT - scenario.colision.h && meneghetti.colision.y > (SCREEN_HEIGHT / 2) - 16) {
+            scenario.colision.y -= speed;
+        }
+        else if(s_pressed && meneghetti.colision.y < SCREEN_HEIGHT - meneghetti.colision.h) {
+            meneghetti.colision.y += speed;
+        }
+        if(a_pressed && scenario.colision.x < 0 && meneghetti.colision.x < (SCREEN_WIDTH / 2) - 10) {
+            scenario.colision.x += speed;
+        }
+        else if(a_pressed && meneghetti.colision.x > 0) {
+            meneghetti.colision.x -= speed;
+        }
+        if(d_pressed && scenario.colision.x > SCREEN_WIDTH - scenario.colision.w && meneghetti.colision.x > (SCREEN_WIDTH / 2) - 10) {
+            scenario.colision.x -= speed;
+        }
+        else if(d_pressed && meneghetti.colision.x < SCREEN_WIDTH - meneghetti.colision.w) {
+            meneghetti.colision.x += speed;
+        }
 
         SDL_RenderPresent(renderer);
 
