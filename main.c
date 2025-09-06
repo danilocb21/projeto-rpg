c2e16596a91336209db02ee384aaa3df839ac809
Danilo <danilocbjp@hotmail.com>
2025-09-05 22:51:15 -0300

    refactor player movement

--- main.c
+++ main.c
@@ -10,10 +10,12 @@
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
 
-#define IMAGE_FLAGS IMG_INIT_PNG
-#define MIXER_FLAGS (MIX_INIT_MP3 & MIX_INIT_OGG)
+#define WINDOW_FLAGS (SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE)
+#define RENDERER_FLAGS (SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC)
+#define IMAGE_FLAGS (IMG_INIT_PNG)
+#define MIXER_FLAGS (MIX_INIT_MP3 | MIX_INIT_OGG)
 
-#define GAME_TITLE "C-Tale"
+#define GAME_TITLE "C-Tale: Meneghetti Vs Python"
 
 typedef struct {
     SDL_Window *window;
@@ -22,6 +24,8 @@ typedef struct {
 typedef struct {
     SDL_Texture *texture;
     SDL_Rect colision;
+    int sprite_vel;
+    const Uint8 *keystate;
 } Character;
 
 bool sdl_initialize(Game *game);
@@ -29,6 +33,7 @@ bool sdl_initialize(Game *game);
 void object_cleanup(Character *obj);
 void game_cleanup(Game *game, int exit_status);
 SDL_Texture *criarTextura(SDL_Renderer *render, const char *dir);
+void sprite_update(Character *scenario, Character *player);
 
 int main(void) {
     Game game = {
@@ -46,20 +51,26 @@ int main(void) {
     Character meneghetti = {
         .texture = criarTextura(game.renderer, "assets/sprites/characters/sprite-meneghetti-stopped.png"),
         .colision = {(SCREEN_WIDTH / 2) - 16, (SCREEN_HEIGHT / 2) - 16, 32, 32},
+        .sprite_vel = 3,
+        .keystate = SDL_GetKeyboardState(NULL),
+    };
+    Character scenario = {
+        .texture = criarTextura(game.renderer, "assets/sprites/scenario/scenario.png"),
+        .colision = {0, -SCREEN_HEIGHT, SCREEN_WIDTH * 2, SCREEN_HEIGHT * 2},
     };
 
     while (running) {
         while (SDL_PollEvent(&event)) {
             switch (event.type) {
             case SDL_QUIT:
-                running = false;
+                running = SDL_FALSE;
                 break;
 
             case SDL_KEYDOWN:
                 switch (event.key.keysym.scancode)
                 {
                 case SDL_SCANCODE_ESCAPE:
-                    running = false;
+                    running = SDL_FALSE;
                     break;
                 default:
                     break;
@@ -70,10 +81,13 @@ int main(void) {
             }
         }
 
+        sprite_update(&scenario, &meneghetti);
+
         SDL_SetRenderDrawColor(game.renderer, 0, 0, 0, 255);
 
         SDL_RenderClear(game.renderer);
 
+        SDL_RenderCopy(game.renderer, scenario.texture, NULL, &scenario.colision);
         SDL_RenderCopy(game.renderer, meneghetti.texture, NULL, &meneghetti.colision);
 
         SDL_RenderPresent(game.renderer);
@@ -82,6 +96,7 @@ int main(void) {
     }
 
     object_cleanup(&meneghetti);
+    object_cleanup(&scenario);
     game_cleanup(&game, EXIT_SUCCESS);
     return 0;
 }
@@ -165,4 +180,33 @@ SDL_Texture *criarTextura(SDL_Renderer *render, const char *dir) {
     }
     SDL_FreeSurface(surface);
     return texture;
+}
+
+void sprite_update(Character *scenario, Character *player) {
+    const Uint8 *keys = player->keystate ? player->keystate : SDL_GetKeyboardState(NULL);
+    int speed = player->sprite_vel;
+
+    if (keys[SDL_SCANCODE_W] && scenario->colision.y < 0 && player->colision.y < (SCREEN_HEIGHT / 2) - 16) {
+        scenario->colision.y += speed;
+    } else if (keys[SDL_SCANCODE_W] && player->colision.y > 0) {
+        player->colision.y -= speed;
+    }
+
+    if (keys[SDL_SCANCODE_S] && scenario->colision.y > SCREEN_HEIGHT - scenario->colision.h && player->colision.y > (SCREEN_HEIGHT / 2) - 16) {
+        scenario->colision.y -= speed;
+    } else if (keys[SDL_SCANCODE_S] && player->colision.y < SCREEN_HEIGHT - player->colision.h) {
+        player->colision.y += speed;
+    }
+
+    if (keys[SDL_SCANCODE_A] && scenario->colision.x < 0 && player->colision.x < (SCREEN_WIDTH / 2) - 10) {
+        scenario->colision.x += speed;
+    } else if (keys[SDL_SCANCODE_A] && player->colision.x > 0) {
+        player->colision.x -= speed;
+    }
+
+    if (keys[SDL_SCANCODE_D] && scenario->colision.x > SCREEN_WIDTH - scenario->colision.w && player->colision.x > (SCREEN_WIDTH / 2) - 10) {
+        scenario->colision.x -= speed;
+    } else if (keys[SDL_SCANCODE_D] && player->colision.x < SCREEN_WIDTH - player->colision.w) {
+        player->colision.x += speed;
+    }
 }
